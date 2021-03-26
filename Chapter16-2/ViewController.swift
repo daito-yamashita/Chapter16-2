6934974e25aafb2b8b447ddb8ad80054fa6b1536
ns-yamashita <daito.yamashita@na-s.jp>
2021-03-26 16:09:15 +0900

    Viewの回転

--- Chapter16-2/ViewController.swift
+++ Chapter16-2/ViewController.swift
@@ -9,6 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tombo: UIImageView!
+    
+    var lastRotation: CGFloat = 0.0
+    
+    @IBAction func rotateTombo(_ sender: UIRotationGestureRecognizer) {
+        switch sender.state {
+        case .began:
+            sender.rotation = lastRotation
+        case .changed:
+            tombo.transform = CGAffineTransform(rotationAngle: sender.rotation)
+        case .ended:
+            lastRotation = sender.rotation
+        default:
+            break
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
